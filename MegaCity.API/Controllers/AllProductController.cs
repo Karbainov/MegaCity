60223bea54ce4791120bd1c1c77ff12957b4cb9c
Farid <abuzeyn@mail.ru>
2023-05-03 22:48:02 +0400

    Add all product controller

---
+++ MegaCity.API/Controllers/AllProductController.cs
@@ -0,0 +1,44 @@
+﻿using MegaCity.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MegaCity.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AllProductController : ControllerBase
+    {
+        [HttpGet("product")]
+        public IActionResult GetAllProducts()
+        {
+            List<ProductOutputModel> products = new List<ProductOutputModel>()
+            {
+                new ProductOutputModel()
+                {
+                    Id=5,
+                    Name = "productOne",
+                    Price = 100,
+                    Count=150
+                },
+
+                new ProductOutputModel()
+                {
+                    Id=8,
+                    Name = "productTwo",
+                    Price = 200,
+                    Count=60
+                },
+
+                new ProductOutputModel()
+                {
+                    Id=3,
+                    Name = "productThree",
+                    Price = 300,
+                    Count=50
+                }
+            };
+
+            return Ok(products);
+        }
+    }
+}
