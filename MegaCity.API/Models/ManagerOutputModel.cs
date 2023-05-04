4ab38ebc23be4f41c1de0b11a66587364e3a3672
Farid <abuzeyn@mail.ru>
2023-05-05 02:32:49 +0400

    Add Manager Model

--- MegaCity.API/Models/ManagerOutputModel.cs
+++ MegaCity.API/Models/ManagerOutputModel.cs
@@ -1,21 +1,19 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace MegaCity.API.Models
+﻿namespace MegaCity.API.Models
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class ManagerOutputModel : ControllerBase
+    public class ManagerOutputModel
     {
-        public int Id { get; set; } 
-
+        public int Id { get; set; }
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
-              
-        public int Age { get; set; }
 
-        public int PhoneNumber { get; set; }
+        public int Age { get; set; }
+        
+        public int PhoneNumber { get; set; }   
 
     }
 }
+
+
+                
+
