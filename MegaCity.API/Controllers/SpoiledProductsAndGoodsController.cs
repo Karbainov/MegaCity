6fecfdd2eb1985b4ef8a48a5a1555f1545c4d5e3
Farid <abuzeyn@mail.ru>
2023-05-05 03:22:39 +0400

    Add Spoiled Controller

---
+++ MegaCity.API/Controllers/SpoiledProductsAndGoodsController.cs
@@ -0,0 +1,49 @@
+﻿using MegaCity.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MegaCity.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SpoiledProductsAndGoodsController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult GetAllSpoiledProductAndGoods()
+        {
+            List<SpoiledProductAndGoodsOutputModel> spoiled = new List<SpoiledProductAndGoodsOutputModel>()
+            {
+                new SpoiledProductAndGoodsOutputModel()
+                {
+                    Id=5,
+                    Name = "productOne",
+                    Price = 100,
+                    Count=150,
+                    DataWriteOff="11/12/12",
+                    ReasonWriteOff="lalala"
+                },
+
+                new SpoiledProductAndGoodsOutputModel()
+                {
+                    Id=8,
+                    Name = "productTwo",
+                    Price = 200,
+                    Count=60,
+                    DataWriteOff="11/12/12",
+                    ReasonWriteOff="lalala"
+                },
+
+                new SpoiledProductAndGoodsOutputModel()
+                {
+                    Id=3,
+                    Name = "productThree",
+                    Price = 300,
+                    Count=50,
+                    DataWriteOff="11/12/12",
+                    ReasonWriteOff="lalala"
+                }
+            };
+            return Ok(spoiled);
+        }
+    }
+}
