d29012dd13f71a7353d4e142ee07c0021e38450f
LeylaN <nabiyeval910@gmail.com>
2023-05-04 11:58:45 +0400

    Create goods controller (1)

--- MegaCity.API/Controllers/GoodsController.cs
+++ MegaCity.API/Controllers/GoodsController.cs
@@ -1,11 +1,44 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MegaCity.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MegaCity.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class GoodsController : ControllerBase
     {
+        [HttpGet("All-Goods")]
+        public IActionResult GetAllGoods()
+        {
+            List<GoodsOutputModel> allGoods = new List<GoodsOutputModel>
+            {
+                new GoodsOutputModel
+                {
+                    Name = "goodsOne",
+                    Price = 12.6
+                },
+
+                new GoodsOutputModel
+                {
+                    Name = "goodsTwo",
+                    Price = 19
+                }
+            };
+
+            return Ok(allGoods);
+        }
+
+        [HttpGet("goods")]
+        public IActionResult GetGoods()
+        {
+            GoodsOutputModel goods = new GoodsOutputModel()
+            {
+                Name = "Potato",
+                Price = 17
+            };
+
+            return Ok("goods");
+        }
     }
 }
--- MegaCity.API/Models/GoodsOutputModel.cs
+++ MegaCity.API/Models/GoodsOutputModel.cs
@@ -6,7 +6,7 @@
 
         public string Name { get; set; }
 
-        public int Price { get; set; }
+        public double Price { get; set; }
 
         public int Count { get; set; }
     }
