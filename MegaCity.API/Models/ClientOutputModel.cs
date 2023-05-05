e6715e56330b43ab01d25401050162d6dbfa626b
Shahnaz24 <shahnazavazova@gmail.com>
2023-05-05 19:21:23 +0400

    Create ClientOutoutModel

---
+++ MegaCity.API/Models/ClientOutputModel.cs
@@ -0,0 +1,13 @@
+﻿   namespace MegaCity.API.Models
+    {
+        public class ClientOutputModel
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int ID { get; set; }
+            public int PhoneNumber { get; set; }
+            public string Email { get; set; }
+        }
+    }
+
+
