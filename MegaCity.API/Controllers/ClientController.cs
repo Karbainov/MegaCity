adc662c77e1b8fe3e27d1fd5c1e962f38c38c8be
LeylaN <nabiyeval910@gmail.com>
2023-05-06 01:15:59 +0400

    Create client controller and add end points

---
+++ MegaCity.API/Controllers/ClientController.cs
@@ -0,0 +1,86 @@
+﻿using MegaCity.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MegaCity.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        [HttpGet("clients")]
+        public IActionResult GetAllClients()
+        {
+            List<ClientOutputModel> clients = new List<ClientOutputModel>()
+            {
+                new ClientOutputModel
+                {
+                    FirstName = "Aysel",
+                    LastName = "Memmedova",
+                    Date = "11.05.1992"
+                },
+
+                 new ClientOutputModel
+                {
+                    FirstName = "Emin",
+                    LastName = "Memmedov",
+                    Date = "07.03.1994"
+                }
+            };
+
+            return Ok(clients);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetClient()
+        {
+            ClientOutputModel client = new ClientOutputModel()
+            {
+                FirstName = "Samire",
+                LastName = "Musayeva",
+                Date = "03.01.2002"
+            };
+
+            return Ok(client);
+        }
+
+        [HttpPost()]
+        public IActionResult AddClient(ClientInputModel client)
+        {
+            ClientOutputModel newClient = new ClientOutputModel()
+            {
+                Id = 127,
+                FirstName = client.FirstName,
+                LastName = client.LastName,
+                Date  = client.Date,
+                PhoneNumber = client.PhoneNumber,
+                Email = client.Email
+            };
+
+            return Created("ClientOne", "client");
+        }
+
+        [HttpDelete("/{id}")]
+        public IActionResult DeleteCientById(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("/{id}")]
+        public IActionResult UpdateClient(int id, ClientInputModel client)
+        {
+            ClientOutputModel clientOutput = new ClientOutputModel()
+            {
+                Id = id,
+                FirstName = client.FirstName,
+                LastName = client.LastName,
+                Date = client.Date,
+                PhoneNumber = client.PhoneNumber,
+                Email = client.Email
+            };
+
+            return Ok(clientOutput);
+        }
+    }
+}
---
+++ MegaCity.API/Models/ClientOutputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MegaCity.API.Models
+{
+    public class ClientOutputModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Date { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+}
