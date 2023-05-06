ee2a3a177191034748d8c7a0e7162a94d26e93ae
LeylaN <nabiyeval910@gmail.com>
2023-05-07 01:19:17 +0400

    Create menu controller and models

---
+++ MegaCity.API/Models/MenuInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MegaCity.API.Models
+{
+    public class MenuInputModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Consist { get; set; }
+
+        public double Portion { get; set; }
+    }
+}
---
+++ MegaCity.API/Models/MenuOutputModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MegaCity.API.Models
+{
+    public class MenuOutputModel
+    {
+        public string Name { get; set; }
+
+        public string Consist { get; set; }
+
+        public double Portion { get; set; }
+    }
+}
