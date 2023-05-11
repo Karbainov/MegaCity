f3baf51786529793cabaae7012f379b9b52b02c6
El_makso <El_makso@mail.ru>
2023-05-11 18:34:59 +0300

    fix

--- MegaCity.API/Models/OutputModels/CashboxOutputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MegaCity.API.Models.OutputModel
-{
-    public class CashboxOutputModel
-    {
-        public double Cash { get; set; }
-
-        public double Card { get; set; }
-    }
-}
