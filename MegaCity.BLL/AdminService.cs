9d97bfbbf7a2c7527c4650c264c979000ba269b8
LeylaN <nabiyeval910@gmail.com>
2023-05-12 19:03:42 +0400

    1.21 Add admins and managers mappers

---
+++ MegaCity.BLL/AdminService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MegaCity.BLL.Models;
+
+namespace MegaCity.BLL
+{
+    public class AdminService
+    {
+        public List<AdminModel> GetAllAdmins()
+        {
+            List<AdminModel> admins = new List<AdminModel>()
+            {
+                new AdminModel()
+                {
+                    FirstName = "FirstName",
+                    LastName = "LastName",
+                    Age=150,
+                    PhoneNumber=123456
+                },
+
+                new AdminModel()
+                {
+                    FirstName = "FirstName",
+                    LastName = "LastName",
+                    Age=150,
+                    PhoneNumber=123452
+                },
+
+                new AdminModel()
+                {
+                    FirstName = "FirstName",
+                    LastName = "LastName",
+                    Age=50,
+                    PhoneNumber=123458
+                }
+            };
+
+            return admins;
+        }
+    }
+}
