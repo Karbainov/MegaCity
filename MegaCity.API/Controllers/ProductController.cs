95b25c8a8a97e658f3572772721e14af8c157421
Rena Amirova <renaamirova336@gmail.com>
2023-05-12 13:19:08 +0400

    Add Lay For Products

--- MegaCity.API/Controllers/ProductController.cs
+++ MegaCity.API/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
 ﻿using MegaCity.API.Models.ModelsOutput;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MegaCity.BLL;
+using MegaCity.BLL.Models;
+using AutoMapper;
 
 namespace MegaCity.API.Controllers
 {
@@ -8,38 +11,28 @@ namespace MegaCity.API.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        ProductService _productService;
+        Mapper _mapper;
+
+        public ProductController()
+        {
+            _productService = new ProductService();
+
+            MapperConfiguration configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MapperApiProfile());
+            });
+            _mapper = new Mapper(configuration);
+        }
         [HttpGet()]
         public IActionResult GetAllProducts()
         {
-            List<ProductResponseModel> products = new List<ProductResponseModel>()
-            {
-                new ProductResponseModel()
-                {
-                    Id=5,
-                    Name = "productOne",
-                    Price = 100,
-                    Count=150
-                },
-
-                new ProductResponseModel()
-                {
-                    Id=8,
-                    Name = "productTwo",
-                    Price = 200,
-                    Count=60
-                },
-
-                new ProductResponseModel()
-                {
-                    Id=3,
-                    Name = "productThree",
-                    Price = 300,
-                    Count=50
-                }
-            };
-
+            List<ProductModel> product = _productService.GetAllProducts();
+            List<ProductResponseModel> products = _mapper.Map<List<ProductResponseModel>>(product);
             return Ok(products);
-        }
+
+        } 
+
 
         [HttpGet("{id}")]
         public IActionResult GetProductById(int id)
---
+++ MegaCity.BLL/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCity.BLL.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public int Count { get; set; }
+    }
+}
