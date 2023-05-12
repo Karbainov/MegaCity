47e958dd292874ed7a960052d33c1a7c1888f3db
LeylaN <nabiyeval910@gmail.com>
2023-05-12 19:04:46 +0400

    add am=dmins and managers mapper

--- MegaCity.BLL/ManagerService.cs
+++ MegaCity.BLL/ManagerService.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MegaCity.BLL.Models;
 
 namespace MegaCity.BLL
 {
-    internal class ManagerService
+    public class ManagerService
     {
+        public List<ManagerModel> GetAllManagers()
+        {
+            List<ManagerModel> managers = new List<ManagerModel>()
+            {
+                new ManagerModel()
+                {
+                    Id=5,
+                    FirstName = "FirstName",
+                    LastName = "LastName",
+                    Age=150,
+                    PhoneNumber=123456
+                },
+
+                new ManagerModel()
+                {
+                    Id=6,
+                    FirstName = "FirstName",
+                    LastName = "LastName",
+                    Age=150,
+                    PhoneNumber=123452
+                },
+
+                new ManagerModel()
+                {
+                    Id=44,
+                    FirstName = "FirstName",
+                    LastName = "LastName",
+                    Age=150,
+                    PhoneNumber=123458
+                }
+            };
+
+            return managers;
+        }
     }
 }
