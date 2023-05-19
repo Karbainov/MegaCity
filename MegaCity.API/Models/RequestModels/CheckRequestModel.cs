d462f17a50130f0e7ad8ae7b2e5de445efa21508
Shahnaz24 <shahnazavazova@gmail.com>
2023-05-19 14:21:35 +0400

    Add check

---
+++ MegaCity.API/Models/RequestModels/CheckRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using MegaCity.API.Models.ResponseModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MegaCity.API.Models.RequestModels
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckRequestModel : ControllerBase
+    {
+        public double Sum { get; set; }
+
+        public List<ProductResponseModel> Products { get; set; }
+    }
+}
