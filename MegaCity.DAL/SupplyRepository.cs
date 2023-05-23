edec743f90e36c7086f4016c4fac58ff2e951a45
LeylaN <nabiyeval910@gmail.com>
2023-05-23 18:51:49 +0400

    Separated Supply and WriteOff Controllers

---
+++ MegaCity.DAL/SupplyRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MegaCity.DAL.Dots;
+
+namespace MegaCity.DAL
+{
+    public class SupplyRepository
+    {
+        private MegaCityDbContext _context;
+
+        public SupplyRepository()
+        {
+            _context = new MegaCityDbContext();
+        }
+
+        public void GetAllSupply()
+        {
+            _context.StorageChanges.ToList();
+        }
+
+        public void AddSupply(StorageChangeDto supply)
+        {
+            _context.StorageChanges.Add(supply);
+            _context.SaveChanges();
+        }
+
+        public void DeleteSupplyById(int id)
+        {
+            var supply = _context.StorageChanges.FirstOrDefault(i => i.Id == id);
+            if (supply != null)
+            {
+                _context.StorageChanges.Remove(supply);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
